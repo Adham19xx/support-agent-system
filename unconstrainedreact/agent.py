87417f010024aa8dace0480cc4def1039759b731
GannaNasr <gana.nasr.eng@gmail.com>
2026-07-24 12:15:54 +0300

    Update agent.py

--- unconstrainedreact/agent.py
+++ unconstrainedreact/agent.py
@@ -19,3 +19,19 @@ tools = [
     search_bug_database,
     create_ticket
 ]
+
+def run_agent(user_message):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=user_message,
+        config={
+            "tools": tools
+        }
+    )
+
+
+    for part in response.candidates[0].content.parts:
+
+        if part.function_call:
+            print("Gemini wants to use a tool")

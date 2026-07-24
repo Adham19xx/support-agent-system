8c9d1a2c84558dc890aeb1374121e063f5e62be1
GannaNasr <gana.nasr.eng@gmail.com>
2026-07-24 12:14:06 +0300

    Update agent.py

--- unconstrainedreact/agent.py
+++ unconstrainedreact/agent.py
@@ -1,1 +1,21 @@
+from google import genai
 
+from tools import (
+    get_user_info,
+    search_bug_database,
+    create_ticket
+)
+
+import os
+
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+
+tools = [
+    get_user_info,
+    search_bug_database,
+    create_ticket
+]
